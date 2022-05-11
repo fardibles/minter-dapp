4083c08fefbbb510dafae9e404b22a3dee8d1c82
Ty M <ty.macconnell@gmail.com>
2022-05-11 16:03:50 -0500

    Updated CSS styling

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,24 +1,24 @@
 const contractAddress = "0xAF06120A3180fDeA913C4bCF59CBBaeAB851BC69"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const welcome_h1 = "Welcome, Fardlings";
+const welcome_h2 = "Connect to MetaMask to join the fun!";
+const welcome_p = 'Fardibles are a 5,000-strong species of collectible sentient flatulence living on the Polygon blockchain.💨';
+const h1_presale_coming_soon = "Fardible drop coming soon!";
+const h1_presale_mint = "Pre-sale minting open!";
+const h1_public_mint = "Public minting is now open!";
+const h2_presale_coming_soon = "Countdown to pre-sale";
+const h2_presale_mint = "Countdown to public mint";
+const p_presale_coming_soon = "Fardibles are run by a one-man show. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale 😢";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 💨";
+const p_presale_mint_already_minted = "You've already claimed your whitelist Fardible. Thank you! 💨";
+const p_public_mint = "No whitelist needed. Public minting is now open! 💨";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint your own Fardible";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint your Fardible NFT";
+const button_presale_already_minted = "Join the Fardlings";
+const button_public_mint = "Mint your very own Fardible NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
